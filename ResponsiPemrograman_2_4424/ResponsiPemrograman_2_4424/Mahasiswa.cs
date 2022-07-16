19063f3e961a3cd2e503108654b7900d89e9ccba
Rachonavis <rachonavis_a@students.amikom.ac.id>
2022-07-16 10:06:03 +0700

    responsi

---
+++ ResponsiPemrograman_2_4424/ResponsiPemrograman_2_4424/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman_2_4424
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public float IPK { get; set; }
+
+    }
+}
